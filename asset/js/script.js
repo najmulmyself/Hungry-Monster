192c17a13d628944805f116e943e7cc7c4ad0132
Najmul Huda <najmul.myself@gmail.com>
2021-02-13 18:44:01 +0600

    div appended

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,18 +1,29 @@
-// fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
-// .then(res => res.json())
-// .then(data => {
-//     const meals = data.meals;
-//     console.log(meals);
-//     for (let i = 0; i < meals.length; i++) {
-//         const mealsArray = meals[i];
-//         console.log(mealsArray.strMeal)
-        
-//     }
-// } 
-//     );
-
-
-const submit = document.getElementById('submit').addEventListener('click', () => {
+const search = document.getElementById('search');
+search.addEventListener('click',getInput)
+function getInput() {
     const input = document.getElementById('input-field').value;
-    console.log(input);
-})
+    
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=`)
+    .then(res => res.json())
+    .then(data => {
+            const mealsdata = data.meals;
+            const mainDiv = document.getElementById('meal-area');
+            for (let i = 0; i < mealsdata.length; i++) {
+                const mealName = mealsdata[i];
+                // const mealId = mealName.idMeal;
+                const strMeal = mealName.strMeal;
+                const separateDiv = document.createElement('div');
+                const h2 = document.createElement('h2');
+                const h5 = document.createElement('h5');
+                h2.innerText = `Food: ${strMeal}`;
+                h5.innerText = `Id: ${mealName.idMeal}`;
+
+                separateDiv.appendChild(h5);
+                separateDiv.appendChild(h2);
+                mainDiv.appendChild(separateDiv);
+
+                
+            }
+        }
+    )
+}
